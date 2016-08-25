44cf99918eb9cd67754827ad7bb2fac20ba6b780
haslunjo <haslunjo@gmail.com>
2016-08-25 14:15:47 -0400

    Update isoFrac.py

--- isoFrac.py
+++ isoFrac.py
@@ -67,11 +67,12 @@ X, infodict = integrate.odeint(dX_dt,X0,t,full_output=True);
 print(infodict['message'])
 print(X)
 
-#plt.plot(t,X.T[0])
-#plt.xlabel("Time")
-#plt.ylabel("NO2 concentration")
-#p.savefig('NO2_of_t.png', bbox_inches='tight')
-#plt.figure(1)
+plt.figure(1)
+plt.plot(t,X.T[0])
+plt.xlabel("Time")
+plt.ylabel("NO2 concentration")
+p.savefig('NO2_of_t.png', bbox_inches='tight')
+
 
 #plt.clf()
 #plt.plot(t,X.T[1],'r')
